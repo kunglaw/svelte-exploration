0c2e8131ea7726e2a0d6f1cedfd64631a0a8facb
kunglaw <alhusna901@gmail.com>
2020-06-28 16:10:32 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Test from "./Test.svelte";
+  export let name;
+
+  function changename(event) {
+    name = event.target.value;
+  }
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <p>parent component :</p>
+  <h1>Hello {name}!</h1>
+  <Test {name} changeName={changename} />
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
---
+++ src/Test.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let name; // ini artinya props yang dijadikan state
+  export let changeName;
+
+  let color = "green";
+
+  function helloPerson() {
+    alert("Hello " + name);
+  }
+</script>
+
+<main>
+  <p>child component :</p>
+  <h1 style="color:{color};font-style:italic">Dio Says : {name}</h1>
+  <input type="text" name="name" bind:value={name} on:keyup={changeName} />
+  <select bind:value={color}>
+    <option>red</option>
+    <option>green</option>
+    <option>blue</option>
+  </select>
+  <button on:click={helloPerson}>Greet this person</button>
+</main>
